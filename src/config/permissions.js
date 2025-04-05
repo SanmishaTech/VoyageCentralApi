dee936973ccfb2bffd6e39205b50b2fd3583158b
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-04-05 12:15:55 +0530

    Update permissions to include super_admin role for user and role management

--- src/config/permissions.js
+++ src/config/permissions.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "roles.read": ["admin"],
-  "users.read": ["admin", "user"],
-  "users.write": ["admin"],
-  "users.delete": ["admin"],
-  "users.export": ["admin"],
+  "roles.read": ["admin", "super_admin"],
+  "users.read": ["admin", "user", "super_admin"],
+  "users.write": ["admin", "super_admin"],
+  "users.delete": ["admin", "super_admin"],
+  "users.export": ["admin", "super_admin"],
   "packages.read": ["super_admin"], // Permission to read packages
   "packages.write": ["super_admin"], // Permission to create or update packages
   "packages.delete": ["super_admin"], // Permission to delete packages
