fc81403166561b96d2b2e4a4474b0cb4837c6cbc
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-04-14 15:42:23 +0530

    staff

--- src/config/permissions.js
+++ src/config/permissions.js
@@ -1,31 +1,44 @@
 module.exports = {
   //only superadmin section
+  //users
   "users.read": ["super_admin"],
   "users.write": ["super_admin"],
   "users.delete": ["super_admin"],
   "users.export": ["super_admin"],
+  //agencies
   "agencies.read": ["super_admin"],
   "agencies.write": ["super_admin"],
   "agencies.delete": ["super_admin"],
-  "roles.read": ["super_admin"],
+  //packages
   "packages.read": ["super_admin"],
   "packages.write": ["super_admin"],
   "packages.delete": ["super_admin"],
   "subscriptions.write": ["super_admin"],
   //superAdmin and admin sections
+  //branches
   "branches.read": ["super_admin", "admin"],
   "branches.write": ["super_admin", "admin"],
   "branches.delete": ["super_admin", "admin"],
+  //countries
   "countries.write": ["super_admin", "admin"],
   "countries.delete": ["super_admin", "admin"],
   "countries.read": ["super_admin", "admin"],
+  //states
   "states.write": ["super_admin", "admin"],
   "states.delete": ["super_admin", "admin"],
   "states.read": ["super_admin", "admin"],
+  //cities
   "cities.write": ["super_admin", "admin"],
   "cities.delete": ["super_admin", "admin"],
   "cities.read": ["super_admin", "admin"],
+  //sectors
   "sectors.write": ["super_admin", "admin"],
   "sectors.delete": ["super_admin", "admin"],
   "sectors.read": ["super_admin", "admin"],
+  //roles
+  "roles.read": ["super_admin", "admin"],
+  //staff
+  "staff.read": ["admin"],
+  "staff.write": ["admin"],
+  "staff.delete": ["admin"],
 };
